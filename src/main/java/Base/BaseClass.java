9a87d546639961e5a10d890a035189be137d6f83
Abdulrahman Alkhaled <AAlkhaled@outlook.sa>
2024-07-11 21:54:42 +0300

    Changes in second branch

--- src/main/java/Base/BaseClass.java
+++ src/main/java/Base/BaseClass.java
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 //Abdulrhman
+// This is changes in second branch
 
 public class BaseClass {
     public WebDriver driver;
